ab4016096be23a3931d70d394a3f577c3d008345
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-07-18 22:24:42 -0500

    Atender tickets de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,38 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const divAlerta = document.querySelector('.alert');
 
+const searchParams = new URLSearchParams( window.location.search );
 
-console.log('Escritorio HTML');
+if( !searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = 'none';
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+btnAtender.addEventListener( 'click', () => {
+    socket.emit('atender-ticket', { escritorio }, ( { ok, ticket} ) => {
+        if( !ok ) {
+            lblTicket.innerText = 'Nadie';
+            return divAlerta.style.display = '';
+        }
+
+        lblTicket.innerText = 'Ticket ' + ticket.numero;
+    });
+});
